aa83064ca22f6f9edad9da72367b62668bb05ddf
piomuc000 <piomuc000@o365.student.pbs.edu.pl>
2023-03-30 15:51:21 +0200

    Zrobilem to, apka wygląda nieźle, jeszcze nie odpaliłem na emulatorze, ide nie znajduje problemów

--- app/src/main/java/pl/edu/pbs/apka8_lab3_v2/MainActivity.java
+++ app/src/main/java/pl/edu/pbs/apka8_lab3_v2/MainActivity.java
@@ -1,14 +1,82 @@
 package pl.edu.pbs.apka8_lab3_v2;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private TextView mXTextView;
+    private TextView mYTextView;
+    private TextView mZTextView;
+    private ListView mSensorListView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSensorListView = findViewById(R.id.sensor_list);
+        mXTextView = findViewById(R.id.x_value);
+        mYTextView = findViewById(R.id.y_value);
+        mZTextView = findViewById(R.id.z_value);
+
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        List<Sensor> sensorList = mSensorManager.getSensorList(Sensor.TYPE_ALL);
+
+        ArrayList<String> sensorNames = new ArrayList<>();
+        for (Sensor sensor : sensorList) {
+            sensorNames.add(sensor.getName());
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, sensorNames);
+        mSensorListView.setAdapter(adapter);
+
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float acc_x = event.values[0];
+            float acc_y = event.values[1];
+            float acc_z = event.values[2];
+
+            mXTextView.setText(String.format(getString(R.string.accel_string), "X", acc_x));
+            mYTextView.setText(String.format(getString(R.string.accel_string), "Y", acc_y));
+            mZTextView.setText(String.format(getString(R.string.accel_string), "Z", acc_z));
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Niepotrzebne w tym przypadku
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
     }
 }
